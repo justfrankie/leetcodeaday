dc7005d17190c54dea77843286c2e77ff5e3524f
justfrankie <zhuangfrankie@gmail.com>
2020-11-17 23:15:55 -0800

    added reversestring

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -1,14 +1,18 @@
 class Stack {
-    constructor() {
-        this.storage = {}
-    }
-
-    push(item){
-       this.storage[item] = item
-    }
-
+   constructor(){
+       this.storage = {}
+        this.index = 0;
+        this.push = function(item){
+            this.storage[this.index] = item;     
+         }
+         //this.pop
+         //this.size
+         //this.peek
+   }
+   
 }
 
 
 let newStack = new Stack();
 newStack.push("hello")
+console.log(newStack)
