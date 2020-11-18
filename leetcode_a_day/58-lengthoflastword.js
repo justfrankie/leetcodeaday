fa92a9677b8ecc7d89085e8a68b9f3e817d0daf1
justfrankie <zhuangfrankie@gmail.com>
2020-11-17 23:46:27 -0800

    added test for lengthoflastword

--- leetcode_a_day/58-lengthoflastword.js
+++ leetcode_a_day/58-lengthoflastword.js
@@ -1,4 +1,4 @@
-/**
+/*
 58. Length of Last Word
  Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
 
@@ -11,18 +11,29 @@ Example:
 Input: "Hello World"
 Output: 5
 
- * @param {string} s
+ * @param {string}
  * @return {number}
  */
- 
-var lengthOfLastWord = function(s) {
-    let split = s.split(" ")
-    for (let i = split.length-1; i >= 0; i--) {
-        if (split[i]) {
-           return split[i].length 
-        }
-    }
 
-    return 0
+let lengthOfLastWord = (s) => {
+  if (s.length === 0) {
+    return 0;
+  }
 
+  let split = s.trim().split(" "); // split all words by spaces, without
+  return new Number(split[split.length - 1].length);
 };
+
+let assertEquals = (actual, expected, message) => {
+  // if (actual.length !== expected.length ) return false;
+    if (actual === expected) {
+      console.log(`X - FAILED Actual: ${actual} Expected: ${expected}!`);
+      return;
+    } else {
+      console.log(`PASSED! ${message} PASSED! `);
+      return;
+    }
+};
+
+assertEquals(lengthOfLastWord("this is a sentence"), 8, "Should return 8");
+assertEquals(lengthOfLastWord("a "), 1, "Should return 1");
