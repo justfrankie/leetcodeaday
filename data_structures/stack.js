3810f97c18d57bd293d095a49665b7e6e67789d4
Frankie H. Liu <53983131+justfrankie@users.noreply.github.com>
2022-07-24 21:33:47 -0700

    write test cases for stack

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -1,31 +1,41 @@
 // Stack has LIFO (last in first out) order
-
 class Stack {
   constructor(){
-      this.storage = []
+      this.storage = new Array;
   }
-  push (item) {this.storage.push(item);  }
+  push (item) {this.storage.push(item) }
   pop () { return this.storage.pop()}
   top () { return this.storage[this.storage.length-1]}
-  isEmpty  () { return !!this.storage.length}
+  isEmpty  () { return !this.storage.length ? true : false}
   size () { return this.storage.length}
 }
 
-const assertEquals = (expected, actual) => {
-  console.log("Expected " + expected + " === " + actual);
-  console.log(expected == actual);
-}
-
 const stack = new Stack();
-stack.push("tiger")
+
 // stack.push("tiger") stack.storage() will return [tiger]
-assertEquals(["tiger"], stack.storage)
+stack.push("tiger")
+console.log(`${JSON.stringify(stack.storage)} === ["tiger"]`)
 
 // stack.push("panda") stack.storage() will return [tiger, panda]
 stack.push("panda")
-assertEquals(["tiger", "panda"], stack.storage)
+console.log(`${JSON.stringify(stack.storage)} === ["tiger", "panda"]`)
+
 // stack.top() will return panda
+stack.top()
+console.log(`${JSON.stringify(stack.top())} === "panda"`)
+
 // stack.size() will return 2
-// stack.pop() stack.storage() will return [tiger]
+stack.size()
+console.log(`${stack.size()} === 2`)
+
+// stack.pop() stack.storage() will return ["tiger"]
+stack.pop()
+console.log(`${JSON.stringify(stack.storage)} === ["tiger"]`)
+
 // stack.pop() stack.storage() will return []
+stack.pop()
+console.log(`${JSON.stringify(stack.storage)} === []`)
+
 // stack.isEmpty() will return true
+stack.isEmpty()
+console.log(`${stack.isEmpty()} === true`)
