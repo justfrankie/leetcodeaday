5b7f9fff522a17dafc2c7ab3c657b95cc0c1f7f6
Frankie H. Liu <53983131+justfrankie@users.noreply.github.com>
2022-07-24 19:42:44 -0700

    refractor stack DS with tests

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -1,18 +1,31 @@
+// Stack has LIFO (last in first out) order
+
 class Stack {
-   constructor(){
-       this.storage = {}
-        this.index = 0;
-        this.push = function(item){
-            this.storage[this.index] = item;     
-         }
-         //this.pop
-         //this.size
-         //this.peek
-   }
-   
+  constructor(){
+      this.storage = []
+  }
+  push (item) {this.storage.push(item);  }
+  pop () { return this.storage.pop()}
+  top () { return this.storage[this.storage.length-1]}
+  isEmpty  () { return !!this.storage.length}
+  size () { return this.storage.length}
 }
 
+const assertEquals = (expected, actual) => {
+  console.log("Expected " + expected + " === " + actual);
+  console.log(expected == actual);
+}
 
-let newStack = new Stack();
-newStack.push("hello")
-console.log(newStack)
+const stack = new Stack();
+stack.push("tiger")
+// stack.push("tiger") stack.storage() will return [tiger]
+assertEquals(["tiger"], stack.storage)
+
+// stack.push("panda") stack.storage() will return [tiger, panda]
+stack.push("panda")
+assertEquals(["tiger", "panda"], stack.storage)
+// stack.top() will return panda
+// stack.size() will return 2
+// stack.pop() stack.storage() will return [tiger]
+// stack.pop() stack.storage() will return []
+// stack.isEmpty() will return true
