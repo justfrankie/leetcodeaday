f3d83a41782d520fe3c0c2b6665382676a8083d0
Frankie H. Liu <53983131+justfrankie@users.noreply.github.com>
2023-07-19 16:11:35 -0700

    Update number-of-subarrarys.js

--- leetcode_a_day/number-of-subarrarys.js
+++ leetcode_a_day/number-of-subarrarys.js
@@ -63,6 +63,66 @@ function readLine() {
  *  2. INTEGER k
  */
 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countNumberOfSubarrays' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. INTEGER k
+ */
+
+// Solution 1: brute force with two pointers in two loops
+
+// function countNumberOfSubarrays(arr, k) {
+//     // Write your code here
+//     // [1, 2, 3, 0]
+//     // [0, 0, 0, 1]
+
+//     // create count variable = 0
+//     let count = 0;
+//     for (let a = 0; a < arr.length; a++) {
+//         // create pointer a as arr[i]
+//         let sum = 0;
+//         // create a second loop, with b as the second pointer
+//         for (let b = a; b < arr.length; b++) {
+//         // move the variable b down the subsequent numbers, keep adding to the current value as long as its below to k
+//             sum += arr[b]
+//             // if results = k, add count ++
+//             if (sum === k) {
+//                     count++
+//                 }
+//             }
+//     }
+//     // return count
+//     return count
+// }
+
+// Solution 2: Map 
 function countNumberOfSubarrays(arr, k) {
     // Write your code here
     // [1, 2, 3, 0]
@@ -70,18 +130,28 @@ function countNumberOfSubarrays(arr, k) {
 
     // create count variable = 0
     let count = 0;
+    // create sum variable
+    let sum = 0;
+    
+    // create map of sum
+    const sumMap = new Map();
+    // initializes map with {0: 1}
+    sumMap.set(0, 1) 
+    
+    // iterate over arr with for loop
     for (let a = 0; a < arr.length; a++) {
-        // create pointer a as arr[i]
-        let sum = 0;
-        // create a second loop, with b as the second pointer
-        for (let b = a; b < arr.length; b++) {
-        // move the variable b down the subsequent numbers, keep adding to the current value as long as its below to k
-            sum += arr[b]
-            // if results = k, add count ++
-            if (sum === k) {
-                    count++
-                }
-            }
+        // accumulate sum and the current number
+        sum += arr[a];
+        // check if map has the difference between the current sum value and k value
+        if (sumMap.has(sum - k)) {
+            count += sumMap.get(sum - k)
+        }
+        if (sumMap.has(sum)) {
+            sumMap.set(sum, sumMap.get(sum)+1);
+        } else {
+            // if doesn't have the sum in the array, set the key as sum, value as 1;
+            sumMap.set(sum, 1);
+        }
     }
     // return count
     return count
@@ -112,3 +182,4 @@ function main() {
 
     ws.end();
 }
+
