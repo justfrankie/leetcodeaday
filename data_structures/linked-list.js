60453b86a9b374f4081db5ce68ce1dadaac32973
justfrankie <zhuangfrankie@gmail.com>
2024-02-04 21:43:18 -0800

    init

--- data_structures/linked-list.js
+++ data_structures/linked-list.js
@@ -12,99 +12,20 @@ class LinkedList {
   }
 
   // insert first node
-  insertFirst(data) {
-    this.head = new Node(data, this.head);
-    this.size++;
-  }
+  insertFirst(data) {}
 
   // insert last node
-  insertLast(data) {
-    let node = new Node(data);
-    let current;
-
-    // if empty, make head
-    if (!this.head) {
-      this.head = node;
-    } else {
-      current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-
-      current.next = node;
-    }
-    this.size++;
-  }
+  insertLast(data) {}
 
   // insert at index
-  insertAtIndex(data, index) {
-    // if index is out of range
-    if (index > 0 && index > this.size) {
-      return;
-    }
-
-    if (index === 0) {
-      this.head = new Node(data, this.head);
-      return;
-    }
-
-    const node = new Node(data);
-    let current, previous;
-
-    // set current to first
-    current = this.head;
-    let count = 0;
-    // loop over the linked list
-    while (count < index) {
-      previous = current; // node before index
-      count++;
-      current = current.next; // node after index
-    }
-
-    node.next = current;
-    previous.next = node;
-  }
+  insertAtIndex(data, index) {}
 
   // get at index
-  getAtIndex(index) {
-    let current = this.head;
-    let count = 0;
-
-    while (current) {
-      if (count === index) {
-        console.log(current.data);
-      }
-      count++;
-      current = current.next;
-    }
-
-    return null;
-  }
+  getAtIndex(index) {}
 
   // Remove at index
   removeAtIndex(index) {
-    // out of range, return
-    if (index > 0 && index > this.size) {
-      return;
-    }
-    // set marker flag
-    let current = this.head;
-    let previous;
-    let count = 0;
-
-    // if first, remove first
-    if (index === 0) {
-      this.head = current.next;
-    } else {
-      while (count < index) {
-        count++;
-        previous = current;
-        current = current.next;
-      }
-      previous.next = current.next;
-    }
-
-    this.size--;
+    // out of range, return;
   }
 
   // clear list
@@ -115,33 +36,10 @@ class LinkedList {
 
   getNthNodeFromLast(index) {
     // if index is out of range, return;
-    if (index > this.size) return;
-
-    // if no this.size, we have to run through the linkedList first to set size
-
-    // create count variable by this.size - index
-    let count = this.size - index;
-    let current = this.head;
-
-    // iterate over linkedList up until the count variable
-    while (current) {
-      if (count === 0) {
-        console.log(current.data);
-      }
-      count--;
-      current = current.next;
-    }
   }
 
   // print list data
-  printListData() {
-    let current = this.head;
-
-    while (current) {
-      console.log(current.data);
-      current = current.next;
-    }
-  }
+  printListData() {}
 }
 
 // example usage:
@@ -156,6 +54,6 @@ ll.insertFirst(100);
 // ll.insertAtIndex(500, 2);
 // ll.removeAtIndex(1);
 // ll.clear();
-ll.getNthNodeFromLast(3); // should return 200
-// ll.printListData();
+// ll.getNthNodeFromLast(3); // should return 200
 // ll.getAtIndex(2);
+ll.printListData();
